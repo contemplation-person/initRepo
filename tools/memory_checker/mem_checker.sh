7aceb4aa3752c64b5f4edfe5198512f26457f225
Ha Ju Young <contemplation.person@gmail.com>
2025-02-12 11:52:06 +0900

    [Refec] using ps -eo cmd,rss

--- tools/memory_checker/mem_checker.sh
+++ tools/memory_checker/mem_checker.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 set -e
 
-CURRENT_DIR=$(pwd)
-CULUMS_LINE="$(top -b -n 1 |head -n 7)"
+ROOT_DIR=$(pwd)
 DIR_NAME="$(/bin/date "+%Y-%m-%d")"
 FILE_NAME="$(/bin/date "+%Y-%m-%d_%H")"
-
 PROC_SET="
-zsh 
+zsh
 su
+sh
 "
+$(echo "CMD RSS(kb)" > "$ROOT_DIR/$DIR_NAME/$FILE_NAME")
+mkdir -p "$ROOT_DIR/$DIR_NAME"
 
-mkdir -p "$CURRENT_DIR/$DIR_NAME"
-
-capture_top_data_from() {
-    trim_word=$(echo $@ | awk '{$1=$1; print}')
-
-    echo "$CULUMS_LINE" > $DIR_NAME/$FILE_NAME
-    top -b -n 1 | grep -E "$(echo "$trim_word" | tr ' ' '|')" >> "$DIR_NAME/$FILE_NAME"
-}
-
-capture_top_data_from $PROC_SET
+for proc in $PROC_SET; do    
+    ps -eo cmd,rss | awk -v proc="$proc" '$1 == proc && NF==2 {print $1, $2}' >> "$ROOT_DIR/$DIR_NAME/$FILE_NAME"
+done
